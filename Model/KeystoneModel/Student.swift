698025232a920b6ac55883bbc151ab2c7c387659
Todd Olsen <todd.olsen@me.com>
2015-11-29 20:39:22 -0500

    Add Note entity

--- Model/KeystoneModel/Student.swift
+++ Model/KeystoneModel/Student.swift
@@ -9,17 +9,17 @@
 import Foundation
 import CoreData
 
-public final class Student: ManagedObject {
-
-
-}
+public final class Student: ManagedObject { }
 
 extension Student {
     
-    @NSManaged public private(set) var firstName: String
-    @NSManaged public private(set) var lastName: String
-    @NSManaged public private(set) var graduationDate: NSDate
+    @NSManaged public var firstName: String
+    @NSManaged public var lastName: String
+    @NSManaged public var graduationDate: NSDate
+    @NSManaged public var image: NSData?
     @NSManaged public private(set) var assignments: Set<Assignment>
+    @NSManaged public private(set) var notes: Set<Note>
+    
 }
 
 extension Student {
@@ -33,6 +33,17 @@ extension Student {
     
     public func addAssignments<S: SequenceType where S.Generator.Element == Assignment>(newAssignments: S) {
         assignments.unionInPlace(newAssignments)
+    }
+    
+    public func newNote() -> Note {
+        let note: Note = managedObjectContext!.insertObject()
+        note.timestamp = NSDate()
+        notes.insert(note)
+        return note
+    }
+    
+    public func removeNote(note: Note) {
+        notes.remove(note)
     }
 }
 
@@ -66,4 +77,21 @@ extension Student {
         student.graduationDate  = graduationDate
         return student
     }
+}
+
+public final class Note: ManagedObject { }
+
+extension Note {
+    
+    @NSManaged public var text: NSAttributedString?
+    @NSManaged public var timestamp: NSDate
+    
+}
+
+extension Note: ManagedObjectType {
+    
+    public static var entityName: String { return "Note" }
+    public static var defaultSortDescriptors: [NSSortDescriptor] { return [NSSortDescriptor(key: "timestamp", ascending: true)] }
+    public static var defaultPredicate: NSPredicate { return NSPredicate() }
+    
 }
